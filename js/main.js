f72f4157b570c43f2837a5de672469d86f07f961
winentravel <winentravel@naver.com>
2021-08-23 16:49:18 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
-const searchEL = document.querySelector('.search');
-const searchInputEl = searchEL.querySelector('input');
 
-searchEL.addEventListener('click', function (){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-  searchEL.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEL.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -149,7 +134,3 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
